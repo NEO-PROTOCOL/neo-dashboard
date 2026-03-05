440b6395181967369cab4d2d5276da4b54d7495f
MELLO <neo@neoprotocol.space>
2026-03-05 14:32:24 -0300

    fix: add CSP header allowing unsafe-eval for 3D graph libs

--- server.js
+++ server.js
@@ -163,6 +163,7 @@ app.use((req, res, next) => {
         res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
         res.setHeader('Pragma', 'no-cache');
         res.setHeader('Expires', '0');
+        res.setHeader('Content-Security-Policy', "script-src 'self' 'unsafe-eval' 'unsafe-inline' https: blob:;");
     }
     next();
 });
